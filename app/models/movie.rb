403561b2abe194b4d34e884f25263422094d3bd0
jzlantz <jzlantz@gmail.com>
2015-10-23 13:12:34 +0000

    update with Tmdb search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,19 +2,17 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
-  
+    
   def self.find_in_tmdb(search_terms)
     Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
     begin
       Tmdb::Movie.find(search_terms)
     rescue ArgumentError => tmdb_error
-      raise Movie::InvalidKeyError, tmdb_error.message
+      
     rescue RuntimeError => tmdb_error
-      if tmdb_error.message =~ /status code '404'/
-        raise Movie::InvalidKeyError, tmdb_error.message
-      else
-        raise RuntimeError, tmdb_error.message
-      end
+      
+    rescue NoMethodError => e
+
     end
   end
   
